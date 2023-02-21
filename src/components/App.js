52013799c04bf786c0661091466e99b4398477bb
kekskiller <kekskiller@email.com>
2023-02-21 19:53:41 +0100

    implemented NewTweet

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
-import { handleInitialData } from "../actions/shared"
-// import Dashboar d from "./Dashboard";
-import NewTweet from "./NewTweet"
-import LoadingBar from "react-redux-loading-bar"
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import { LoadingBar } from "react-redux-loading-bar";
 
 const App = (props) => {
-  useEffect(()=> {
-    props.dispatch(handleInitialData())
-  },[])
-
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
   return (
     <div>
       <LoadingBar />
-      {props.loading === true ? null : <NewTweet />}
-    </div> 
-
+      {props.loading === true ? null : (
+        <TweetPage match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }} />
+      )}
+    </div>
   );
-}
-
-const mapStateToProps = ({authedUser}) => ({
-  loading: true === null,
-})
-
-export default connect()(App);
+};
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
+export default connect(mapStateToProps)(App);
